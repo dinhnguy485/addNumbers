c44f90f7154bc4ae0ec10b17d71d5a9790a7de44
Tri Nguyen <Tri Nguyen@WINDOWS-PAMP5OC>
2024-02-29 14:33:14 -0800

    Add project files.

---
+++ addNumbers/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace addNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.numInput = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Location = new System.Drawing.Point(95, 73);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(79, 13);
+            this.numberLabel.TabIndex = 0;
+            this.numberLabel.Text = "Enter a number";
+            // 
+            // numInput
+            // 
+            this.numInput.Location = new System.Drawing.Point(210, 66);
+            this.numInput.Name = "numInput";
+            this.numInput.Size = new System.Drawing.Size(100, 20);
+            this.numInput.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(156, 117);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(143, 181);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(16, 13);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "...";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(156, 222);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 288);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.numInput);
+            this.Controls.Add(this.numberLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.TextBox numInput;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ addNumbers/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace addNumbers
+{
+    public partial class Form1 : Form
+    {
+        int num;
+        double sum;
+        int number;
+        
+        public Form1()
+        {
+            number = 0;
+            sum = 0;
+            InitializeComponent();
+            resetButton.Enabled = false;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num = Convert.ToInt32(numInput.Text);
+                if (num != 0)
+                {
+                    number++;
+                    sum = sum + num;
+                    outputLabel.Text = $"{num} was added to the sum";
+                }
+                else
+                {
+                    resetButton.Enabled = true;
+                    sum = (sum + 0) / number;
+                    outputLabel.Text = $"{sum} is the average ";
+                }
+            }
+            catch (FormatException)
+            {
+                resetButton.Enabled = true;
+
+                MessageBox.Show("please enter a valid number");
+            }
+
+
+            
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            num = 0;
+            sum = 0;
+            number = 0;
+            outputLabel.Text = " ";
+            numInput.Text = " ";
+            resetButton.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
